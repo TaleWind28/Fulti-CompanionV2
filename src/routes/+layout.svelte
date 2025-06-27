7d48f449eb4e30daf2aa1cb1b0eaf8674d01a30b
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-06-27 17:00:40 +0200

    prova disposizione header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,45 @@
 <script lang="ts">
-  import { NavigationMenuContent, NavigationMenuItem, NavigationMenuTrigger } from '$lib/components/ui/navigation-menu';
+  import {NavigationMenuItem} from '$lib/components/ui/navigation-menu';
   import NavigationMenuLink from '$lib/components/ui/navigation-menu/navigation-menu-link.svelte';
   import NavigationMenuList from '$lib/components/ui/navigation-menu/navigation-menu-list.svelte';
   import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
   import NavigationMenu from '$lib/components/ui/navigation-menu/navigation-menu.svelte';
+  import { Button } from 'bits-ui';
 	import '../app.css';
 	let { children } = $props();
 	let poweredSrc = "src/images/Powered by Fabula Ultima Logo.png" ;
-	
-	let headerLinks =[ {link:"/",name:"Home"},{link:"/",name:"Campagne"},{link:"/",name:"Schede Personaggio"},{link:"/",name:"Generatore di Oggetti"},{link:"/",name:"Bestiario"}];
+	let logo = "src/images/Logo5.1.png";
+	let headerLinks =[{link:"/",name:"Campagne"},{link:"/",name:"Schede Personaggio"},{link:"/",name:"Generatore di Oggetti"},{link:"/",name:"Bestiario"}];
 </script>
 
 <div class="flex flex-col h-full bg-caribbean_current-500">
-	<header>
-		<NavigationMenu class="w-full">
-			<NavigationMenuList class="flex items-center justify-between bg-caribbean_current-500 w-full">
-				{#each headerLinks as page }
-					<NavigationMenuItem class="flex-1">
-						<NavigationMenuLink class="w-full">
-							{#snippet child()}
-								<a href={page.link} class="{navigationMenuTriggerStyle()} !bg-caribbean_current-500 hover:!bg-caribbean_current-400 text-white text-3xl" >{page.name}</a>
-							{/snippet}
-						</NavigationMenuLink>
-					</NavigationMenuItem>	
-				{/each}
-				
+	<header class="flex items-center justify-center">
+		<NavigationMenu class="w-full p-5">
+			<NavigationMenuList class="flex  justify-between items-center gap-5 bg-caribbean_current-500 w-full">
+				<NavigationMenuItem class="flex-none">
+					<NavigationMenuLink class="flex items-start">
+						<a href="/"> <img src={logo} alt="logo" class="flex items-start  w-auto h-12"></a>
+					</NavigationMenuLink>
+				</NavigationMenuItem>
+
+				<div class="flex space-x-4 justify-center">
+					{#each headerLinks as page }
+						<NavigationMenuItem class="!flex-1 !justify-end">
+							<NavigationMenuLink class="w-full">
+								{#snippet child()}
+									<a href={page.link} class="{navigationMenuTriggerStyle()} !bg-caribbean_current-500 hover:!bg-caribbean_current-400 text-white text-3xl !w-full !h-full !text-center" >{page.name}</a>
+								{/snippet}
+							</NavigationMenuLink>
+						</NavigationMenuItem>	
+					{/each}
+				</div>
+
+				<NavigationMenuItem class="">
+					<NavigationMenuLink>
+						<a href="/login">Accedi/Registrati</a>
+					</NavigationMenuLink>
+				</NavigationMenuItem>
+			
 			</NavigationMenuList>
 		</NavigationMenu>
 	</header>
