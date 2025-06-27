cb54c69753ce43e04bdb6705cc66d5ef307ff73d
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-06-27 11:02:49 +0200

    Prova con get e post

---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+
+	let a = 0;
+	let b = 0;
+	let total = 0;
+
+	async function add() {
+		const response = await fetch('/api/add', {
+			method: 'POST',
+			body: JSON.stringify({ a, b }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		total = await response.json();
+	}
+</script>
+
+<input type="number" bind:value={a}> +
+<input type="number" bind:value={b}> =
+{total}
+
+<Button onclick={add} class="bg-black text-white">Calculate</Button>
