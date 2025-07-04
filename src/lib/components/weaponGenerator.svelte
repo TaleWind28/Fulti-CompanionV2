a3e1f4a27e092065be0ac53a620afc5a1c1430d7
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-07-04 12:04:37 +0200

    styling weapongenerator

--- src/lib/components/weaponGenerator.svelte
+++ src/lib/components/weaponGenerator.svelte
@@ -5,6 +5,7 @@
     import Checkbox from "./ui/checkbox/checkbox.svelte";
     import Label from "./ui/label/label.svelte";
     import Textarea from "./ui/textarea/textarea.svelte";
+    import CardFooter from "./ui/card/card-footer.svelte";
     
     //fetchare db
     const fruits = [
@@ -22,11 +23,11 @@
     );
 </script>
 
-<Card.Root class="w-200">
-    <Card.Header>
-        <Card.Title>WeaponGenerator</Card.Title>
+<Card.Root class="w-200 bg-cafe_noir-700 border-0">
+    <Card.Header class="">
+        <Card.Title>Generatore di Armi  </Card.Title>
     </Card.Header>
-    <Card.Content class="flex flex-col gap-5">
+    <Card.Content class=" py-5 flex flex-col gap-5 bg-white">
         <!-- Prima riga: Arma base, Nome e Marziale -->
         <div class="flex flex-row gap-5">
             <Select.Root type="single" name="favoriteFruit" bind:value>
@@ -59,20 +60,129 @@
             <Input type="text" placeholder="Nome Arma"/>
         </div>
         <!-- Seconda Riga: Tipo di danno, Numero di Mani ed Attributi di precisione  -->
-        <div>
-
+        <div class="flex flex-row gap-5">
+            <Select.Root type="single" name="favoriteFruit" bind:value>
+                <Select.Trigger class="w-[180px]">
+                    {triggerContent}
+                </Select.Trigger>
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Fruits</Select.Label>
+                        {#each fruits as fruit (fruit.value)}
+                            <Select.Item
+                            value={fruit.value}
+                            label={fruit.label}
+                            disabled={fruit.value === "grapes"}
+                            >
+                                {fruit.label}
+                            </Select.Item>
+                        {/each}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+            <Select.Root type="single" name="favoriteFruit" bind:value>
+                <Select.Trigger class="w-[180px]">
+                    {triggerContent}
+                </Select.Trigger>
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Fruits</Select.Label>
+                        {#each fruits as fruit (fruit.value)}
+                            <Select.Item
+                            value={fruit.value}
+                            label={fruit.label}
+                            disabled={fruit.value === "grapes"}
+                            >
+                                {fruit.label}
+                            </Select.Item>
+                        {/each}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+            <Select.Root type="single" name="favoriteFruit" bind:value>
+                <Select.Trigger class="w-[180px]">
+                    {triggerContent}
+                </Select.Trigger>
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Fruits</Select.Label>
+                        {#each fruits as fruit (fruit.value)}
+                            <Select.Item
+                            value={fruit.value}
+                            label={fruit.label}
+                            disabled={fruit.value === "grapes"}
+                            >
+                                {fruit.label}
+                            </Select.Item>
+                        {/each}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+            <Select.Root type="single" name="favoriteFruit" bind:value>
+                <Select.Trigger class="w-[180px]">
+                    {triggerContent}
+                </Select.Trigger>
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Fruits</Select.Label>
+                        {#each fruits as fruit (fruit.value)}
+                            <Select.Item
+                            value={fruit.value}
+                            label={fruit.label}
+                            disabled={fruit.value === "grapes"}
+                            >
+                                {fruit.label}
+                            </Select.Item>
+                        {/each}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
         </div>
 
         <!-- Terza Riga: Qualità Standard, Modificatori Precisione e Danno -->
-        <div>
+        <div class="flex flex-row justify-between">
+            <Select.Root type="single" name="favoriteFruit" bind:value>
+                <Select.Trigger class="w-[180px]">
+                    {triggerContent}
+                </Select.Trigger>
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Fruits</Select.Label>
+                        {#each fruits as fruit (fruit.value)}
+                            <Select.Item
+                            value={fruit.value}
+                            label={fruit.label}
+                            disabled={fruit.value === "grapes"}
+                            >
+                                {fruit.label}
+                            </Select.Item>
+                        {/each}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+            <div class="flex flex-col">
+                <span class="flex flex-row">
+                    <Checkbox id="damageMod"></Checkbox>
+                    <Label for="damageMod">+4 Danno</Label>
+                </span>
+                <span class="flex flex-row">
+                    <Checkbox id="accuracyMod"></Checkbox>
+                    <Label for="accuracymod">+1 Precisione</Label>
+                </span>
 
+            </div>
         </div>
         
         <!-- Quarta riga: Qualità Custom e prezzo della Qualità -->
-        <div>
+        <div class="flex flex-row gap-5">
             <Textarea>
 
             </Textarea>
+            <span class="flex flex-col gap-5 w-30 items-center">
+                <Label for="price">Costo Qualità</Label>
+                <Input id="price" type="number" class="w-20"/>  
+            </span>
+            
         </div>
     </Card.Content>
 </Card.Root>
--- src/routes/itemGenerator/+page.svelte
+++ src/routes/itemGenerator/+page.svelte
@@ -4,6 +4,6 @@
     
 </script>
 
-<div class="p-5">
+<div class="p-5 bg-cafe_noir-900">
     <WeaponGenerator></WeaponGenerator>
 </div>
