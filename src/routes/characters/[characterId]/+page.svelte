f00e2d2619c3947d3174e6541f647ac43de7982a
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-08-13 22:21:07 +0200

    risolto, effect serve esattamente a quello

--- src/routes/characters/[characterId]/+page.svelte
+++ src/routes/characters/[characterId]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import { invalidate, invalidateAll } from '$app/navigation';
+    import { invalidateAll } from '$app/navigation';
     import CharacterCard from '$lib/components/characterCard.svelte';
-    import { Content } from '$lib/components/ui/card/index.js';
     import * as Tabs from '$lib/components/ui/tabs/index.js';
 
 	let { data } = $props();
 
 	let character = $state(data.character);
 
-	let tabSelector = $derived(
+	let tabSelector = $derived.by(()=>
+	(
 		{
 			triggers:[
 				{value:"sheet",text:character.name},
@@ -36,14 +36,16 @@
 
 		}
 	)
+)
 	let tabValue = $state("sheet");
-	$inspect(tabValue,"tab",character,"personaggio",tabSelector.contents[0].props.character.traits,"Contenuto del componente");
+	$inspect(tabValue,"tab",character,"personaggio",tabSelector.contents[0].props.character,data.character.traits);
+	$effect(()=>{character = data.character});
 </script>
 
 
 <div class="bg-cafe_noir-900 flex items-center justify-center p-10">
-	<Tabs.Root bind:value={tabValue} onValueChange={async ()=>{await invalidateAll();console.log("awaited",tabSelector.contents[0].props.character.traits)}}>
-		<Tabs.List class="bg-cafe_noir-700 gap-5"> 
+	<Tabs.Root bind:value={tabValue} onValueChange={async ()=>{await invalidateAll();console.log("awaited")}}>
+		<Tabs.List class="bg-cafe_noir-700 gap-5">
 			{#each tabSelector.triggers as trigger}
 				<Tabs.Trigger value={trigger.value} class="bg-cafe_noir-700  data-[state=active]:bg-cafe_noir text-white"> 
 					{trigger.text}
