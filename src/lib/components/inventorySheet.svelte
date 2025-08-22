4c7ca1962912c0b3ddb77fa20ef50f2151410ecc
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-08-22 14:37:55 +0200

    aggiunta prima prova per funzionamento dialog + generatori

--- src/lib/components/inventorySheet.svelte
+++ src/lib/components/inventorySheet.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-    import { faArrowDown, faArrowUp, faGem, faKhanda, faShield, faUserShield } from "@fortawesome/free-solid-svg-icons";
+    import { faArrowDown, faArrowUp, faGem, faKhanda, faShield, faShieldAlt, faShieldHalved, faUserShield,type IconDefinition } from "@fortawesome/free-solid-svg-icons";
     import Button from "./ui/button/button.svelte";
     import * as Card from "./ui/card/index";
+    import * as Dialog from "./ui/dialog/index";
     import Fa from "svelte-fa";
     import type { Weapon, Armor, Shield, Accessory } from "$lib";
+    import WeaponGenerator from "./weaponGenerator.svelte";
+    import { Header } from "./ui/table";
+    //props
     let {weapons, shields, armor, accessories}: {weapons:Weapon[],armor:Armor[],shields:Shield[],accessories:Accessory[]} = $props();
+    //dialogVariable
     let openWeaponCreator = $state(false);
     let openArmorCreator = $state(false);
     let openShieldCreator = $state(false);
@@ -14,6 +19,8 @@
     let viewArmor = $state(false);
     let viewShields = $state(false);
     let viewAccessories = $state(false);
+
+    let btnStyle = "bg-cafe_noir-600 hover:bg-cafe_noir-500 flex flex-row items-center";
 </script>
 
 <div class="flex flex-col gap-5 justify-center">
@@ -22,23 +29,11 @@
         <Card.Header> 
             Azioni Rapide
         </Card.Header>
-        <Card.Content class="flex flex-row justify-center gap-5"> 
-            <Button onclick={()=>openWeaponCreator = true} class="bg-cafe_noir-600 hover:bg-cafe_noir-500"> 
-                Nuova Arma
-                <Fa icon={faKhanda}></Fa>
-            </Button>
-            <Button onclick={()=>openArmorCreator = true } class="bg-cafe_noir-600 hover:bg-cafe_noir-500"> 
-                Nuova Armatura
-                <Fa icon={faUserShield}></Fa>
-            </Button>
-            <Button onclick={()=>openShieldCreator = true} class="bg-cafe_noir-600 hover:bg-cafe_noir-500"> 
-                Nuovo Scudo
-                <Fa icon={faShield}></Fa>
-            </Button>
-            <Button onclick={()=>openAccessoryCreator = true} class="bg-cafe_noir-600 hover:bg-cafe_noir-500"> 
-                Nuovo accessorio
-                <Fa icon={faGem}/>
-            </Button>
+        <Card.Content class="flex flex-row items-center justify-center gap-5">
+            {@render actionButton(btnStyle,()=>openWeaponCreator=true,faKhanda,"Nuova Arma")}
+            {@render actionButton(btnStyle,()=>openArmorCreator=true,faUserShield,"Nuova Armatura")}
+            {@render actionButton(btnStyle,()=>openShieldCreator=true,faShieldHalved,"Nuovo Scudo")}
+            {@render actionButton(btnStyle,()=>openAccessoryCreator=true,faGem,"Nuovo Accessorio")}            
         </Card.Content>
     </Card.Root>
 
@@ -50,6 +45,12 @@
         {@render inventoryPiece("Accessori", accessories, viewAccessories, () => viewAccessories = !viewAccessories)}
     </div>
 </div>
+
+<Dialog.Root open={openWeaponCreator} onOpenChange={(v)=> openWeaponCreator=v}>
+    <Dialog.Content class="flex items-center justify-center w-700">
+        <WeaponGenerator showImageProcessor={false} dim={"w-120"}></WeaponGenerator>
+    </Dialog.Content>
+</Dialog.Root>
 
 
 {#snippet inventoryPiece(name: string, inventory: Array<Weapon|Armor|Shield|Accessory>, showContent: boolean, toggleFn: () => void)}
@@ -67,10 +68,7 @@
         {#if showContent}
             <Card.Content class="bg-white">
                 {#each inventory as item}
-                    <div class="p-2 border-b">
-                        <!-- Qui puoi aggiungere la visualizzazione dell'item -->
-                        {JSON.stringify(item)}
-                    </div>
+                    <p>{item.name}</p>
                 {/each}
                 {#if inventory.length === 0}
                     <p class="text-gray-500 p-4">Nessun elemento presente</p>
@@ -78,4 +76,11 @@
             </Card.Content>
         {/if}
     </Card.Root>
+{/snippet}
+
+{#snippet actionButton(styleClass:string,clickFun: ()=>void,icon:IconDefinition, description:string)} 
+    <Button onclick={clickFun} class={styleClass}> 
+        <p>{description}</p>
+        <Fa icon={icon}/>
+    </Button>
 {/snippet}
--- src/lib/components/weaponGenerator.svelte
+++ src/lib/components/weaponGenerator.svelte
@@ -15,6 +15,7 @@
     import { WeaponScheme } from "$lib/zod";
     import { toast } from "svelte-sonner";
     
+    let {showImageProcessor = true, dim ="w-150"} = $props();
     // Fetch dei dati iniziali
     onMount(async () => {
         try {
@@ -245,7 +246,7 @@
 <div class="flex flex-row gap-5 justify-evenly">
 
     <!-- Generatore -->
-    <Card.Root class="w-150 bg-cafe_noir-700 border-0">
+    <Card.Root class="{dim} bg-cafe_noir-700 border-0">
         <Card.Header class="">
             <Card.Title>Generatore di Armi  </Card.Title>
         </Card.Header>
@@ -431,62 +432,64 @@
     </Card.Root>
 
     <!-- ImageProcesor -->
-    <div>
-        <div  id={"arma"} class="bg-white border-black h-auto ">
-            <!-- Intestazione tabella -->
-            <div class="bg-cafe_noir-700 grid grid-cols-6">
-                <p class="col-span-1 px-2">
-                    {weaponName}
-                    {#if isMartial}
-                        <span class="text-red-600 " style="text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">♦</span>
-                    {/if}
-                </p>
-                <span class="grid grid-cols-3  col-span-5 gap-30 px-10">
-                    {#each ["PRECISIONE","DANNO","COSTO"] as header}
-                        <p> {header} </p>
-                    {/each}
+    {#if showImageProcessor}
+        <div>
+            <div  id={"arma"} class="bg-white border-black h-auto ">
+                <!-- Intestazione tabella -->
+                <div class="bg-cafe_noir-700 grid grid-cols-6">
+                    <p class="col-span-1 px-2">
+                        {weaponName}
+                        {#if isMartial}
+                            <span class="text-red-600 " style="text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">♦</span>
+                        {/if}
+                    </p>
+                    <span class="grid grid-cols-3  col-span-5 gap-30 px-10">
+                        {#each ["PRECISIONE","DANNO","COSTO"] as header}
+                            <p> {header} </p>
+                        {/each}
+                    </span>
+                </div>
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <ImageUploader2 padre="weaponGenerator" dimensions={"w-25 h-25 border-r"} fill={true} bind:imageUrl = {weaponImageUrl}/>
+                    </div>
+                    <div class="flex-1">
+                        <div class="justify-around bg-cafe_noir-800 flex ">
+                            {#each calculatedResults.formulaRow as formula}
+                                <p> {formula} </p>
+                            {/each}
+                        </div>
+                        <hr>
+                        <div class="flex flex-row items-center justify-between px-2 ">
+                            {#each calculatedResults.thirdRowElement as element }
+                                <p class="text-center"> {element} </p>
+                            {/each}
+                        </div>
+                        <hr>
+                        <div class="px-2 break-words w-150">
+                            {quality}
+                        </div>
+                    </div>
+                </div>
+            
+                
+            </div>
+            <span class="flex flex-row">
+                <span>
+                    <button onclick={()=>exportHtmlToImage('arma')}>
+                        <Fa icon={faDownload} class="cursor-pointer px-2 w-auto"/>
+                    </button>
                 </span>
-            </div>
-            <div class="flex">
-                <div class="flex-shrink-0">
-                    <ImageUploader2 padre="weaponGenerator" dimensions={"w-25 h-25 border-r"} fill={true} bind:imageUrl = {weaponImageUrl}/>
-                </div>
-                <div class="flex-1">
-                    <div class="justify-around bg-cafe_noir-800 flex ">
-                        {#each calculatedResults.formulaRow as formula}
-                            <p> {formula} </p>
-                        {/each}
-                    </div>
-                    <hr>
-                    <div class="flex flex-row items-center justify-between px-2 ">
-                        {#each calculatedResults.thirdRowElement as element }
-                            <p class="text-center"> {element} </p>
-                        {/each}
-                    </div>
-                    <hr>
-                    <div class="px-2 break-words w-150">
-                        {quality}
-                    </div>
-                </div>
-            </div>
-        
-            
+                
+                <span>
+                    <button onclick={handleExport}>
+                        <Fa icon={faFileExport} class="cursor-pointer px-2 w-auto"></Fa>
+                    </button>
+                </span>
+                
+            </span>
         </div>
-        <span class="flex flex-row">
-            <span>
-                <button onclick={()=>exportHtmlToImage('arma')}>
-                    <Fa icon={faDownload} class="cursor-pointer px-2 w-auto"/>
-                </button>
-            </span>
-            
-            <span>
-                <button onclick={handleExport}>
-                    <Fa icon={faFileExport} class="cursor-pointer px-2 w-auto"></Fa>
-                </button>
-            </span>
-            
-        </span>
-    </div>
+    {/if}
 </div>
 
 
