207bd243cc2e63946d79bacff94d7dae69fc2958
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-08-25 13:18:20 +0200

    styling pagina finito manca la fetch al db e l'aggiornamento della scheda personaggio, però mancano i dati nel db quindi tocca fare quelli

--- src/lib/components/sheets/spellBook.svelte
+++ src/lib/components/sheets/spellBook.svelte
@@ -1,42 +1,49 @@
 <script lang="ts">
-    import type { Spell } from "$lib/zod";
+    import type { Spell, Spellbook } from "$lib/zod";
     import * as Card from "$lib/components/ui/card/index";
     import * as Select from "$lib/components/ui/select/index";
     import * as Dialog from "$lib/components/ui/dialog/index";
     import { buttonVariants } from "../ui/button/index";
     import ScrollArea from "../ui/scroll-area/scroll-area.svelte";
     import Separator from "../ui/separator/separator.svelte";
-    let {spellBook} : {spellBook:Spell} =$props();
+    import SpellDescriptor from "../spellDescriptor.svelte";
 
+    /*  PROP  */
+    let {spellBook} : {spellBook:Spellbook} =$props();
+    /* FINE PROP */
+    
     let selectedSpellClass = $state("");
     let spellClasses = ["Elementalista","Chimerista","Entropista","Spiritista"];
     const spellClassTrigger = $derived(
         spellClasses.find(sp=>sp === selectedSpellClass) || "Nessuna Lista Selezionata"
     );
-
+    
+    /* LO SCRIPT è SOLO PER TESTARE LA PAGINA DOPO VA AGGIORNATO CON I FETCH DAL DB E LE CALLBACK PER AGGIORNARE LA SCHEDA*/
     let spells = [
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Fulgur",description:"soffio gelido"},
-        {name:"Ultimagia",description:"Magia Meno Finale"},
-        {name:"Cometa",description:"Magia Finale"},
+        {name:"Fulgur",description:"Plasmi l'elettricità in un'onda di energia crepitante. Ciascun bersaglio colpito subisce【TM + 15】 danni da fulmine. Opportunità: ciascun bersaglio colpito subisce lo status confuso.",cost:10,targets:"una creatura",duration:"istantanea"},
+        {name:"Ultimagia",description:"Magia Meno Finale",cost:40,targets:"Speciale",duration:"Speciale"},
+        {name:"Cometa",description:"Magia Finale",cost:50,targets:"Speciale",duration:"Speciale"},
     ]
 
-    let selectedSpell = $state("Fulgur");
+    let selectedSpell = $state({name:"Fulgur",description:"Plasmi l’elettricità in un’onda di energia crepitante. Ciascun bersaglio colpito subisce【TM + 15】 danni da fulmine. Opportunità: ciascun bersaglio colpito subisce lo status confuso.",cost:10,targets:"una creatura",duration:"istantanea"});
+    let selectedButton = $state();
 
-    const spellDescription = $derived(
-        spells.find((s)=>s.name === selectedSpell)?.description || "soffio Gelido"
-    )
-    
+
+    let incantesimi:any = $state([  {name:"Fulgur",description:"Plasmi l'elettricità in un'onda di energia crepitante. Ciascun bersaglio colpito subisce【TM + 15】 danni da fulmine. Opportunità: ciascun bersaglio colpito subisce lo status confuso.",cost:10,targets:"una creatura",duration:"istantanea"}]);
+    let acquiredSpellClasses = $state(
+    [
+        {name:"Elementalista", spellList:[...incantesimi]}
+    ])
+
+    function handleSpellAdd(){
+        incantesimi.push(selectedSpell);
+        acquiredSpellClasses[0].spellList = incantesimi;
+    }
 
 </script>
 
-<div>
+<div class="flex flex-col gap-5">
+    <!-- Action Buttons per selezione classe da incantatore ed aggiunta spell -->
     <Card.Root> 
         <Card.Header> 
             Aggiungi un'incantesimo dalle Liste
@@ -66,27 +73,72 @@
 
             <!-- Aggiunta Incantesimo -->
             <Dialog.Root> 
-                <Dialog.Trigger class={buttonVariants({ variant: "outline" })}> 
+                <Dialog.Trigger class={buttonVariants({ variant: "default" })}> 
                     Aggiungi un'incantesimo
                 </Dialog.Trigger>
-                <Dialog.Content> 
-                    <div class="flex flex-row border">
-                        <ScrollArea class="h-80 w-48 rounded-md  gap-5"> 
-                            {#each spells as spell}
-                                <button onclick={()=>selectedSpell=spell.name} class="flex flex-col">
+                <Dialog.Content class="!max-w-none w-250 bg-lion-600"> 
+                    <div class="flex flex-row border bg-white">
+                        <ScrollArea class="h-80 w-50 rounded-md gap-5"> 
+                            {#each spells as spell,index}
+                                <button onclick={()=>{selectedSpell=spell,selectedButton = index}} 
+                                    class="flex flex-col w-full justify-start text-start text-white border {selectedButton === index ? 'bg-lion-300' : 'bg-lion-400'}">
                                     <p>{spell.name}</p>
                                 </button>
                             {/each}
                         </ScrollArea>
                         <Separator orientation="vertical"/>
-                        <div>
-                            <p>
-                                {spellDescription}
-                            </p>
+                        <div class="w-190">
+                            <SpellDescriptor 
+                                name={selectedSpell.name} 
+                                description={selectedSpell.description} 
+                                targets={selectedSpell.targets} 
+                                duration={selectedSpell.duration} 
+                                cost={selectedSpell.cost}
+                            />
                         </div>
                     </div>
+                    <Dialog.Footer> 
+                        <Dialog.Close  class={buttonVariants(
+                                { variant: "default"},
+                                
+                                )}
+                                
+                                onclick={()=>handleSpellAdd}
+                                > 
+                            Aggiungi
+                        </Dialog.Close>
+                    </Dialog.Footer>
                 </Dialog.Content>
             </Dialog.Root>
         </Card.Content>
     </Card.Root>
+
+    <!-- Render delle spell acquisite dal giocatore -->
+    {#each acquiredSpellClasses as sorceries}
+        {@render spellBookSections(sorceries.name,sorceries.spellList)}
+    {/each}
+
+    
 </div>
+
+{#snippet spellBookSections(section:string,incantesimi:any)}
+    <Card.Root class="bg-cafe_noir-600 border-0">  
+        <Card.Header class="text-white font-bold"> 
+            <p>{section}</p>
+        </Card.Header>
+        <Card.Content class="bg-white py-5"> 
+            <div class="w-180 flex flex-col gap-5">
+                {#each incantesimi as spell}
+                    <SpellDescriptor
+                        class="border"
+                        name={spell.name}
+                        description={spell.description}
+                        targets={spell.targets}
+                        duration={spell.duration}
+                        cost={spell.cost}
+                    />
+                {/each}
+            </div>
+        </Card.Content>
+    </Card.Root>
+{/snippet}
---
+++ src/lib/components/spellDescriptor.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { cn } from "$lib/utils";
+    import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
+    import Separator from "./ui/separator/separator.svelte";
+    import Fa from "svelte-fa";
+
+    let { name,isOffensive = true ,cost,targets,duration,description, class:className = "" } = $props();
+    let tableRow = [cost + "MP per bersaglio", "⬥" , targets, "⬥", duration]
+</script>
+
+
+<div class="flex flex-col {cn(className)}">
+    <h1 class="flex flex-col bg-lion-500 ">
+        <span class="flex flex-row items-center gap-2 p-2">
+            <p class="font-bold"> {name} </p>
+            {#if isOffensive}
+                <Fa icon={faBoltLightning} class="text-red-600"/>
+            {/if}
+        </span>
+        
+        <span class="flex flex-row gap-5 items-center justify-center p-2">
+            {#each tableRow as row}
+                <p>{row}</p>
+            {/each}
+        </span>
+    </h1>
+    <Separator orientation="horizontal" class="bg-lion-500"> </Separator>
+    <h2>
+        {description}
+    </h2>   
+</div>
