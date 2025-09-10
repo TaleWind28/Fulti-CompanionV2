40fdbb43539f9e269ca5145fa2ace8d3f60ce5f3
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-09-10 15:45:10 +0200

    primo styling della pagina

--- src/routes/bestiary/[pngId]/+page.svelte
+++ src/routes/bestiary/[pngId]/+page.svelte
@@ -1,13 +1,68 @@
 <script lang="ts">
     import type { PageData } from "../$types";
-
+    import * as Card from '$lib/components/ui/card/index'
     let { data } = $props();
 
 </script>
 
 
-<div>
-    {data.png?.name}
+<div class="flex flex-col mx-50 my-10 gap-5">
+    <!-- Nome, Descrizione Attributi, Livello, Specie -->
+    <Card.Root> 
+        <Card.Header> 
+
+        </Card.Header>
+        <Card.Content> 
+
+        </Card.Content>    
+    </Card.Root>
+
+    <!-- Affinità -->
+     <Card.Root> 
+        <Card.Header> 
+
+        </Card.Header>
+        <Card.Content> 
+
+        </Card.Content>    
+    </Card.Root>
+
+    <!-- Attacchi -->
+     <Card.Root> 
+        <Card.Header> 
+
+        </Card.Header>
+        <Card.Content> 
+
+        </Card.Content>    
+    </Card.Root>
+    <!-- Incantesmi -->
+     <Card.Root> 
+        <Card.Header> 
+
+        </Card.Header>
+        <Card.Content> 
+
+        </Card.Content>    
+    </Card.Root>
+    <!-- Altre Azioni -->
+     <Card.Root> 
+        <Card.Header> 
+
+        </Card.Header>
+        <Card.Content> 
+
+        </Card.Content>    
+    </Card.Root>
+    <!-- Regole Speciali -->
+     <Card.Root> 
+        <Card.Header> 
+
+        </Card.Header>
+        <Card.Content> 
+
+        </Card.Content>    
+    </Card.Root>
 </div>
 
 
